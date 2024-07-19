caafd3f52582c91bf256bd46a08036b290598695
joecrockett5 <joe@crockett.net>
2024-07-19 11:27:23 +0100

    Update favicon

---
+++ src/routes/+layout.ts
@@ -0,0 +1,4 @@
+import { Amplify } from 'aws-amplify';
+import amplifyConfig from '../aws-exports';
+
+Amplify.configure(amplifyConfig);
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Auth } from 'aws-amplify';
+
+	async function login() {
+		const user = await Auth.signIn('jpcrock@outlook.com', 'LaLaLa1234');
+		return user;
+	}
+	console.log(login());
+</script>
+
+<p>login page</p>
