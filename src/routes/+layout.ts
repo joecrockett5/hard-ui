6ba51a08b54b6db8de778d13d5655f3631430e63
joecrockett5 <joe@crockett.net>
2024-08-01 21:23:45 +0100

    Get aws login working

--- src/routes/+layout.ts
+++
@@ -1,4 +0,0 @@
-import { Amplify } from 'aws-amplify';
-import amplifyConfig from '../aws-exports';
-
-Amplify.configure(amplifyConfig);
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Amplify } from 'aws-amplify';
+
+	Amplify.configure({
+		Auth: {
+			Cognito: {
+				userPoolId: 'eu-west-2_g70ekREnm',
+				userPoolClientId: '2pa9i84eu8oflrarbm9v4hg3pc',
+				signUpVerificationMethod: 'code',
+				loginWith: {
+					oauth: {
+						domain: 'hard.auth.eu-west-2.amazoncognito.com',
+						scopes: ['openid', 'email'],
+						redirectSignIn: ['http://localhost:5173'],
+						redirectSignOut: ['http://localhost:5173'],
+						responseType: 'code'
+					}
+				}
+			}
+		}
+	});
+
+	import { signInWithRedirect } from 'aws-amplify/auth';
+
+	function signIn() {
+		console.log('running');
+		signInWithRedirect();
+	}
+</script>
+
+<button on:click={signIn}> Login </button>
