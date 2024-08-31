02fde729a33c4d68784361dcd1fc6acae65fddc3
Joe Crockett <joe@crockett.net>
2024-08-31 12:12:45 +0100

    Update layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,26 @@
 <script>
 	import '../app.css';
+
+	import { Amplify } from 'aws-amplify';
+
+	Amplify.configure({
+		Auth: {
+			Cognito: {
+				userPoolId: process.env.USER_POOL_ID,
+				userPoolClientId: process.env.USER_POOL_CLIENT_ID,
+				signUpVerificationMethod: 'code',
+				loginWith: {
+					oauth: {
+						domain: `${process.env.USER_POOL_DOMAIN}.auth.${process.env.REGION}.amazoncognito.com`,
+						scopes: ['openid', 'email'],
+						redirectSignIn: ['http://localhost:5173'],
+						redirectSignOut: ['http://localhost:5173'],
+						responseType: 'code'
+					}
+				}
+			}
+		}
+	});
 </script>
 
 <slot />
