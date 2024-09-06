5d74de508c5f1bfad165f593fa3d5f334779c1b9
Joe Crockett <joe@crockett.net>
2024-09-06 11:09:21 +0100

    Add test for lambda api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { getCurrentUser } from 'aws-amplify/auth';
+	import { Button } from '$lib/components/ui/button/index.js';
 </script>
 
 <img src="/hard-logo.png" alt="Hard Logo" class="h-64 w-64 mx-auto rounded-xl mt-12 mb-4" />
@@ -7,3 +8,10 @@
 {#await getCurrentUser() then user}
 	<p class="text-2xl mt-4 text-center">Current User: {user.userId}</p>
 {/await}
+
+<Button
+	on:click={async () => {
+		console.log('Fetching workouts');
+		fetch('/api/workouts');
+	}}>Fetch Workouts</Button
+>
--- src/routes/api/workouts/+server.ts
+++ src/routes/api/workouts/+server.ts
@@ -1,5 +1,17 @@
-import { type RequestHandler } from './$types';
+import { HARD_API } from '$env/static/private';
+import { type RequestHandler } from '@sveltejs/kit';
+import { fetchAuthSession } from 'aws-amplify/auth';
 
 export const GET: RequestHandler = async () => {
-	return new Response('Hello World');
+	console.log('GET /api/workouts');
+	const session = await fetchAuthSession();
+	if (!session.tokens?.idToken) {
+		return new Response('Unauthorized', { status: 401 });
+	}
+	const response = await fetch(`${HARD_API}/workouts`, {
+		headers: { Authorization: `Bearer ${session.tokens.idToken}` }
+	});
+	const json = await response.json();
+	console.log(json);
+	return json;
 };
