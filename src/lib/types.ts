44d26d48225cbc774495cd8a4765d561fb1378c7
Joe Crockett <joe@crockett.net>
2024-09-06 09:48:12 +0100

    Update types.ts to use Base

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,9 +1,16 @@
-export type Tag = {
+type Base = {
+	userId: string;
+	timestamp: string;
+	objectId: string;
+	objectType: string;
+};
+
+export type Tag = Base & {
 	name: string;
 	color: string;
 };
 
-export type Set = {
+export type Set = Base & {
 	setType: string;
 	weight: number;
 	weightUnit: string;
@@ -13,14 +20,14 @@ export type Set = {
 	comparison: string;
 };
 
-export type Exercise = {
+export type Exercise = Base & {
 	name: string;
 	description?: string;
 	tags?: Tag[];
 	sets?: Set[];
 };
 
-export type WorkoutInfo = {
+export type WorkoutInfo = Base & {
 	date: string;
 	workoutTitle: string;
 	tags?: Tag[];
