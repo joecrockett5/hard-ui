96221415c816935b189d4b0e544944368dcdf73f
Joe Crockett <joe@crockett.net>
2024-11-01 18:57:02 +0000

    Update GET methods for exercise joins

--- src/routes/api/exercise-joins/+server.ts
+++ src/routes/api/exercise-joins/+server.ts
@@ -1,36 +1,62 @@
 import { HARD_API } from '$env/static/private';
 import { type RequestHandler } from '@sveltejs/kit';
-import { type exercisejoin } from '$lib/types';
+import { type Exercise } from '$lib/types';
 import { getTags, postTags } from '$lib/tag-helpers';
 
 export const GET: RequestHandler = async ({ url }) => {
 	// Need to update backend to allow for filtering by exercisejoin_date
 	// will then add tag support
-	console.log('GET /api/exercisejoins');
+	console.log('GET /api/exercise-joins');
 	const token = url.searchParams.get('token');
 	if (!token) {
 		console.log('Missing token');
 		return new Response('Unauthorized', { status: 401 });
 	}
-	const response = await fetch(`${HARD_API}/exercisejoins`, {
+	const response = await fetch(`${HARD_API}/exercise-joins`, {
 		headers: { Authorization: `Bearer ${token}` }
 	});
-	const rawexercisejoins = await response.json();
-	const exercisejoins: exercisejoin[] = [];
-	rawexercisejoins.forEach((exercisejoin) => {
-		const tags = await getTags(token, exercisejoin.object_id);
-		exercisejoins.push({
-			userId: exercisejoin.user_id,
-			timestamp: exercisejoin.timestamp,
-			objectType: exercisejoin.object_type,
-			objectId: exercisejoin.object_id,
-			exercisejoinDate: exercisejoin.exercisejoin_date,
-			notes: exercisejoin.notes,
-			title: exercisejoin.title,
-			tags
+	const rawExerciseJoins = await response.json();
+	const exercises: Exercise[] = [];
+	rawExerciseJoins.forEach((exerciseJoin) => {
+		const exerciseTags = await getTags(token, exerciseJoin.exercise_id);
+
+		const exerciseResponse = await fetch(`${HARD_API}/exercises/${exerciseJoin.exercise_id}`, {
+			headers: { Authorization: `Bearer ${token}` }
+		});
+		const exerciseData = await exerciseResponse.json();
+
+		const setsResponse = await fetch(`${HARD_API}/sets?exercise=${exerciseJoin.exercise_id}`, {
+			headers: { Authorization: `Bearer ${token}` }
+		});
+		const setsData = await setsResponse.json();
+
+		exercises.push({
+			userId: exerciseJoin.user_id,
+			timestamp: exerciseJoin.timestamp,
+			objectType: exerciseData.object_type,
+			objectId: exerciseData.object_id,
+			joinId: exerciseJoin.object_id,
+			workoutId: exerciseJoin.workout_id,
+			name: exerciseJoin.title,
+			description: exerciseJoin.notes,
+			tags: exerciseTags,
+			sets: setsData.map((set) => {
+				return {
+					userId: set.user_id,
+					timestamp: set.timestamp,
+					objectType: set.object_type,
+					objectId: set.object_id,
+					setType: set.set_type,
+					weight: set.weight,
+					weightUnit: set.weight_unit,
+					reps: set.reps,
+					notes: set.notes,
+					exerciseJoinId: set.exercise_join_id
+				};
+			})
 		});
 	});
-	return new Response(JSON.stringify(exercisejoins), {
+	return new Response(JSON.stringify(exercises), {
 		status: response.status,
 		headers: response.headers
 	});
--- src/routes/api/exercise-joins/[exerciseJoinId]/+server.ts
+++ src/routes/api/exercise-joins/[exerciseJoinId]/+server.ts
@@ -1,30 +1,57 @@
 import { HARD_API } from '$env/static/private';
 import { type RequestHandler } from '@sveltejs/kit';
 import { getTags, putTags, deleteTags } from '$lib/tag-helpers';
-import { type exercisejoin } from '$lib/types';
+import { type Exercise } from '$lib/types';
 
 export const GET: RequestHandler = async ({ url, params }) => {
-	const { exercisejoinId } = params;
-	console.log(`GET /api/exercisejoins/${exercisejoinId}`);
+	const { exerciseJoinId } = params;
+	console.log(`GET /api/exercise-joins/${exerciseJoinId}`);
 	const token = url.searchParams.get('token');
 	if (!token) {
 		console.log('Missing token');
 		return new Response('Unauthorized', { status: 401 });
 	}
-	const response = await fetch(`${HARD_API}/exercisejoins/${exercisejoinId}`, {
+	const response = await fetch(`${HARD_API}/exercise-joins/${exerciseJoinId}`, {
 		headers: { Authorization: `Bearer ${token}` }
 	});
-	const tags = await getTags(token, exercisejoinId);
-	const json = await response.json();
-	const formattedBody: exercisejoin = {
-		userId: json.user_id,
-		timestamp: json.timestamp,
-		objectType: json.object_type,
-		objectId: json.object_id,
-		exercisejoinDate: json.exercisejoin_date,
-		notes: json.notes,
-		title: json.title,
-		tags
+	const exerciseJoin = await response.json();
+
+	const exerciseTags = await getTags(token, exerciseJoin.exercise_id);
+
+	const exerciseResponse = await fetch(`${HARD_API}/exercises/${exerciseJoin.exercise_id}`, {
+		headers: { Authorization: `Bearer ${token}` }
+	});
+	const exerciseData = await exerciseResponse.json();
+
+	const setsResponse = await fetch(`${HARD_API}/sets?exercise=${exerciseJoin.exercise_id}`, {
+		headers: { Authorization: `Bearer ${token}` }
+	});
+	const setsData = await setsResponse.json();
+
+	const formattedBody = {
+		userId: exerciseJoin.user_id,
+		timestamp: exerciseJoin.timestamp,
+		objectType: exerciseData.object_type,
+		objectId: exerciseData.object_id,
+		joinId: exerciseJoin.object_id,
+		workoutId: exerciseJoin.workout_id,
+		name: exerciseJoin.title,
+		description: exerciseJoin.notes,
+		tags: exerciseTags,
+		sets: setsData.map((set) => {
+			return {
+				userId: set.user_id,
+				timestamp: set.timestamp,
+				objectType: set.object_type,
+				objectId: set.object_id,
+				setType: set.set_type,
+				weight: set.weight,
+				weightUnit: set.weight_unit,
+				reps: set.reps,
+				notes: set.notes,
+				exerciseJoinId: set.exercise_join_id
+			};
+		})
 	};
 	return new Response(JSON.stringify(formattedBody), {
 		status: response.status,
