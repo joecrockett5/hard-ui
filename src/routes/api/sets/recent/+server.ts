d50a6be8b698efa88c8382cfdfbfe49c461cb37a
Joe Crockett <joe@crockett.net>
2025-01-26 20:10:06 +0000

    Update mostRecent to recent
    Return all previous instances

--- src/routes/api/sets/recent/+server.ts
+++ src/routes/api/sets/recent/+server.ts
@@ -6,7 +6,7 @@ export const GET: RequestHandler = async ({ url }) => {
 	// Need to update backend to allow for filtering by set_date
 	// will then add tag support
 	const exerciseId = url.searchParams.get('exercise');
-	console.log('GET /api/sets/mostRecent' + (exerciseId ? '?exercise=' + exerciseId : ''));
+	console.log('GET /api/sets/recent' + (exerciseId ? '?exercise=' + exerciseId : ''));
 	const token = url.searchParams.get('token');
 	if (!token) {
 		console.log('Missing token');
@@ -26,35 +26,38 @@ export const GET: RequestHandler = async ({ url }) => {
 	}
 	// order by timestamp desc
 	exJoins.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+	exJoins.shift(); // remove first element (current instance)
+
 	// get sets where exercise_join_id = ex joins
 	const setsResponse = await fetch(`${HARD_API}/sets?exercise=${exerciseId}`, {
 		headers: { Authorization: `Bearer ${token}` }
 	});
 	const rawSets = await setsResponse.json();
-	const relevantSets = rawSets.filter((set) => exJoins[1].object_id === set.exercise_join_id);
-	if (relevantSets.length === 0) {
-		return new Response('No relevant sets found', { status: 404 });
-	}
+	const recentInstances = {};
+	for (const join of exJoins) {
+		let relevantSets = rawSets
+			.filter((set) => join.object_id === set.exercise_join_id)
+			.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
 
-	// order by timestamp asc
-	relevantSets.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
+		const sets: Set[] = [];
+		for (const set of relevantSets) {
+			sets.push({
+				userId: set.user_id,
+				timestamp: set.timestamp,
+				objectType: set.object_type,
+				objectId: set.object_id,
+				setType: set.set_type,
+				weight: set.weight,
+				weightUnit: set.unit,
+				reps: set.reps,
+				notes: set.notes,
+				exerciseJoinId: set.exercise_join_id
+			});
+		}
 
-	const sets: Set[] = [];
-	for (const set of relevantSets) {
-		sets.push({
-			userId: set.user_id,
-			timestamp: set.timestamp,
-			objectType: set.object_type,
-			objectId: set.object_id,
-			setType: set.set_type,
-			weight: set.weight,
-			weightUnit: set.unit,
-			reps: set.reps,
-			notes: set.notes,
-			exerciseJoinId: set.exercise_join_id
-		});
+		recentInstances[join.timestamp] = sets;
 	}
-	return new Response(JSON.stringify(sets), {
+	return new Response(JSON.stringify(recentInstances), {
 		status: 200
 	});
 };
