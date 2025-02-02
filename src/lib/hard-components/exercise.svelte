2270de9365217e5d0dc219e412d0304b6916edf8
Joe Crockett <joe@crockett.net>
2025-02-02 18:39:32 +0000

    Minimize exercise component

--- src/lib/hard-components/exercise.svelte
+++ src/lib/hard-components/exercise.svelte
@@ -8,11 +8,11 @@
 	import NewItem from '$lib/hard-components/new-item.svelte';
 	import type { Exercise, Set } from '$lib/types';
 	import * as Dialog from '$lib/components/ui/dialog';
-	import { buttonVariants } from '$lib/components/ui/button';
+	import { Button, buttonVariants } from '$lib/components/ui/button';
 	import { fetchAuthSession } from 'aws-amplify/auth';
-	import Input from '$lib/components/ui/input/input.svelte';
 	import Skeleton from '$lib/components/ui/skeleton/skeleton.svelte';
 	import { writable } from 'svelte/store';
+	import ExerciseInfoComponent from '$lib/hard-components/exercise-info.svelte';
 
 	export let exercise: Exercise;
 	export let workoutId: string;
@@ -196,115 +196,130 @@
 
 	onMount(fetchOldSets);
 	$: sortedOldInstances = sortOldSets(oldInstances);
+
+	let minimized = true;
+
+	function toggleMinimized() {
+		minimized = !minimized;
+	}
 </script>
 
-<Card.Root class="mt-4">
-	<Dialog.Root>
-		<Dialog.Trigger class={`float-right top-4 right-4 ${buttonVariants({ variant: 'ghost' })}`}
-			>Remove</Dialog.Trigger
-		>
-		<Dialog.Content class="w-3/4">
-			<Dialog.Header>
-				<Dialog.Title>Are you sure you want to remove this exercise?</Dialog.Title>
-				<Dialog.Description>This cannot be undone.</Dialog.Description>
-			</Dialog.Header>
-			<Dialog.Footer>
-				<Dialog.Close class={buttonVariants({ variant: 'ghost' })}>Cancel</Dialog.Close>
-				<Dialog.Close
-					on:click={() => deletionCallback(exercise)}
-					class={buttonVariants({ variant: 'destructive' })}>Remove</Dialog.Close
-				>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</Dialog.Root>
-	<Card.Header class="w-1/2">
-		<Card.Title>
-			{exercise.name}
-		</Card.Title>
-		{#if exercise.description}
-			<Card.Description>{exercise.description}</Card.Description>
-		{/if}
-	</Card.Header>
-	<Card.Content>
-		<div>
-			{#if oldInstances}
-				<Accordion.Root>
-					<Accordion.Item value="previous-sets">
-						<Accordion.Trigger class="w-full mt-0">Previous Sets</Accordion.Trigger>
-						<Accordion.Content class="w-full">
-							{#each sortedOldInstances as sortedInstance}
-								<Accordion.Root>
-									<Accordion.Item value={'previous-set' + sortedInstance.timestamp}>
-										<Accordion.Trigger class="w-full mt-0"
-											>{new Date(sortedInstance.timestamp).toLocaleDateString('en-GB', {
-												day: '2-digit',
-												month: '2-digit',
-												year: '2-digit'
-											})}
-										</Accordion.Trigger>
-										<Accordion.Content class="w-full">
-											{#if sortedInstance.warmup.length > 0}
-												<h4 class="text-l font-bold">Warmup Sets</h4>
-												{#each sortedInstance.warmup as set}
-													<SetComponent {set} deletionCallback={deleteSet} brief />
-												{/each}
-											{/if}
-											{#if sortedInstance.working.length > 0}
-												<h4 class="text-l font-bold mt-2">Working Sets</h4>
-												{#each sortedInstance.working as set}
-													<SetComponent {set} deletionCallback={deleteSet} brief />
-												{/each}
-											{/if}
-										</Accordion.Content>
-									</Accordion.Item>
-								</Accordion.Root>
-							{/each}
-						</Accordion.Content>
-					</Accordion.Item>
-				</Accordion.Root>
-			{/if}
-		</div>
-		<div class="mt-4">
-			<h3 class="text-xl font-bold">Warmup Sets</h3>
-			{#each warmupSets as set}
-				<SetComponent {set} deletionCallback={deleteSet} />
-			{/each}
-			{#if $creatingWarmupSet}
-				<Skeleton class="h-14 w-full mt-2" />
-			{/if}
-			<NewItem item="Warmup Set">
-				<InlineSetEdit
-					bind:reps={warmupSetReps}
-					bind:weight={warmupSetWeight}
-					bind:notes={warmupSetNotes}
-				/>
+{#if !minimized}
+	<Card.Root class="mt-4">
+		<Dialog.Root>
+			<Dialog.Trigger class={`float-right top-4 right-4 ${buttonVariants({ variant: 'ghost' })}`}
+				>Remove</Dialog.Trigger
+			>
+			<Dialog.Content class="w-3/4">
+				<Dialog.Header>
+					<Dialog.Title>Are you sure you want to remove this exercise?</Dialog.Title>
+					<Dialog.Description>This cannot be undone.</Dialog.Description>
+				</Dialog.Header>
 				<Dialog.Footer>
-					<Dialog.Close class={buttonVariants({ variant: 'default' })} on:click={createWarmupSet}
-						>Add</Dialog.Close
+					<Dialog.Close class={buttonVariants({ variant: 'ghost' })}>Cancel</Dialog.Close>
+					<Dialog.Close
+						on:click={() => deletionCallback(exercise)}
+						class={buttonVariants({ variant: 'destructive' })}>Remove</Dialog.Close
 					>
 				</Dialog.Footer>
-			</NewItem>
-		</div>
-		<div class="mt-6">
-			<h3 class="text-xl font-bold">Working Sets</h3>
-			{#each workingSets as set}
-				<SetComponent {set} deletionCallback={deleteSet} />
-			{/each}
-			{#if $creatingWorkingSet}
-				<Skeleton class="h-14 w-full mt-2" />
+			</Dialog.Content>
+		</Dialog.Root>
+		<Button on:click={toggleMinimized} variant="ghost" class="float-right top-4 right-4">
+			Hide
+		</Button>
+		<Card.Header class="w-1/2">
+			<Card.Title>
+				{exercise.name}
+			</Card.Title>
+			{#if exercise.description}
+				<Card.Description>{exercise.description}</Card.Description>
 			{/if}
-			<NewItem item="Working Set">
-				<InlineSetEdit
-					bind:reps={workingSetReps}
-					bind:weight={workingSetWeight}
-					bind:notes={workingSetNotes}
-				/>
-				<Dialog.Footer>
-					<Dialog.Close class={buttonVariants({ variant: 'default' })} on:click={createWorkingSet}
-						>Add</Dialog.Close
-					>
-				</Dialog.Footer>
-			</NewItem>
-		</div>
-	</Card.Content>
-</Card.Root>
+		</Card.Header>
+		<Card.Content>
+			<div>
+				{#if oldInstances}
+					<Accordion.Root>
+						<Accordion.Item value="previous-sets">
+							<Accordion.Trigger class="w-full mt-0">Previous Sets</Accordion.Trigger>
+							<Accordion.Content class="w-full">
+								{#each sortedOldInstances as sortedInstance}
+									<Accordion.Root>
+										<Accordion.Item value={'previous-set' + sortedInstance.timestamp}>
+											<Accordion.Trigger class="w-full mt-0"
+												>{new Date(sortedInstance.timestamp).toLocaleDateString('en-GB', {
+													day: '2-digit',
+													month: '2-digit',
+													year: '2-digit'
+												})}
+											</Accordion.Trigger>
+											<Accordion.Content class="w-full">
+												{#if sortedInstance.warmup.length > 0}
+													<h4 class="text-l font-bold">Warmup Sets</h4>
+													{#each sortedInstance.warmup as set}
+														<SetComponent {set} deletionCallback={deleteSet} brief />
+													{/each}
+												{/if}
+												{#if sortedInstance.working.length > 0}
+													<h4 class="text-l font-bold mt-2">Working Sets</h4>
+													{#each sortedInstance.working as set}
+														<SetComponent {set} deletionCallback={deleteSet} brief />
+													{/each}
+												{/if}
+											</Accordion.Content>
+										</Accordion.Item>
+									</Accordion.Root>
+								{/each}
+							</Accordion.Content>
+						</Accordion.Item>
+					</Accordion.Root>
+				{/if}
+			</div>
+			<div class="mt-4">
+				<h3 class="text-xl font-bold">Warmup Sets</h3>
+				{#each warmupSets as set}
+					<SetComponent {set} deletionCallback={deleteSet} />
+				{/each}
+				{#if $creatingWarmupSet}
+					<Skeleton class="h-14 w-full mt-2" />
+				{/if}
+				<NewItem item="Warmup Set">
+					<InlineSetEdit
+						bind:reps={warmupSetReps}
+						bind:weight={warmupSetWeight}
+						bind:notes={warmupSetNotes}
+					/>
+					<Dialog.Footer>
+						<Dialog.Close class={buttonVariants({ variant: 'default' })} on:click={createWarmupSet}
+							>Add</Dialog.Close
+						>
+					</Dialog.Footer>
+				</NewItem>
+			</div>
+			<div class="mt-6">
+				<h3 class="text-xl font-bold">Working Sets</h3>
+				{#each workingSets as set}
+					<SetComponent {set} deletionCallback={deleteSet} />
+				{/each}
+				{#if $creatingWorkingSet}
+					<Skeleton class="h-14 w-full mt-2" />
+				{/if}
+				<NewItem item="Working Set">
+					<InlineSetEdit
+						bind:reps={workingSetReps}
+						bind:weight={workingSetWeight}
+						bind:notes={workingSetNotes}
+					/>
+					<Dialog.Footer>
+						<Dialog.Close class={buttonVariants({ variant: 'default' })} on:click={createWorkingSet}
+							>Add</Dialog.Close
+						>
+					</Dialog.Footer>
+				</NewItem>
+			</div>
+		</Card.Content>
+	</Card.Root>
+{:else}
+	<button on:click={toggleMinimized} class="w-full text-left">
+		<ExerciseInfoComponent {exercise} brief />
+	</button>
+{/if}
